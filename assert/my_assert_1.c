d5c3165fa39ad06a2798bae51450aaf487b2bd02
liodegwin <liodegwin@gmail.com>
2022-11-03 23:53:39 +0800

    add assert test code

---
+++ assert/my_assert_1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "assert.h"
+
+int main() {
+    assert(2>3);
+    printf("Hello, World!\n");
+    return 0;
+}
--- assert/my_assert_2.c
+++ assert/my_assert_2.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include "assert.h"
 
 int main() {
+    assert(3>2);
     printf("Hello, World!\n");
     return 0;
 }
